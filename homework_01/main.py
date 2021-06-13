e85c270bb7929938c6461f69008f8e3f9f63dc36
lovetotoshka <42470012+lovetotoshka@users.noreply.github.com>
2021-06-13 10:51:34 +0300

    Update main.py

--- homework_01/main.py
+++ homework_01/main.py
@@ -2,15 +2,14 @@
 Домашнее задание №1
 Функции и структуры данных
 """
-"""
-функция, которая принимает N целых чисел,
-и возвращает список квадратов этих чисел
->>> power_numbers(1, 2, 5, 7)
-<<< [1, 4, 25, 49]
-"""
-
 
 def power_numbers(*args):
+    """
+    функция, которая принимает N целых чисел,
+    и возвращает список квадратов этих чисел
+    >>> power_numbers(1, 2, 5, 7)
+    <<< [1, 4, 25, 49]
+    """
     return[val**2 for val in args]
 
 
@@ -21,14 +20,14 @@ PRIME = "prime"
 
 
 def is_prime(num):
-        if num ==1:
-            return False
-        if num % 2 == 0:
-            return num == 2
-        i = 3
-        while i * i <= num and num % i != 0:
-            i += 2
-        return i * i > num
+    if num ==1:
+        return False
+    if num % 2 == 0:
+        return num == 2
+    i = 3
+    while i * i <= num and num % i != 0:
+        i += 2
+    return i * i > num
 
     
 def filter_numbers(lst, param):

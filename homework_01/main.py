358a76d61e26203f17daa46a1fc1c848175157ce
lovetotoshka <42470012+lovetotoshka@users.noreply.github.com>
2021-06-13 08:20:48 +0300

    Update power_numbers

--- homework_01/main.py
+++ homework_01/main.py
@@ -2,23 +2,23 @@
 Домашнее задание №1
 Функции и структуры данных
 """
-def power_numbers(*args):
-    lst = []
-    numbers = args
-    print(numbers)
-    gen = (val**2 for val in numbers)
-    return(list(gen))
+
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
+    
+def power_numbers(*args):
+    return[val**2 for val in args]
+
 
 # filter types
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
+
 
 def is_prime(num):
         if num ==1:
@@ -30,6 +30,7 @@ def is_prime(num):
             i += 2
         return i * i > num
 
+    
 def filter_numbers(lst, param):
     if param == ODD:
         return list(filter(lambda x: x % 2 != 0, lst))

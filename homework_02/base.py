d73ae721609d27d4c3f51e493a93810255fd4006
lovetotoshka <i-shkileva@mail.ru>
2021-07-01 13:14:56 +0300

    create base and exceptions

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,34 @@
-from abc import ABC
+from homework_02 import exceptions
 
 
-class Vehicle(ABC):
-    pass
+class Vehicle:
+
+    started = False
+
+    def __init__(self, weight=100, fuel=100, fuel_consumption=10):
+        self.weight = weight
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+
+    def start(self):
+        if self.started is False:
+            if self.fuel > 0:
+                self.started = True
+            else:
+                raise exceptions.LowFuelError('Low Fuel Error !!!')
+
+    def move(self, distance):
+        if self.fuel > 0 and self.fuel_consumption and self.fuel > self.fuel_consumption:
+            max_distance = self.fuel // self.fuel_consumption
+            if max_distance > distance:
+                self.fuel = self.fuel - distance * self.fuel_consumption
+            else:
+                raise exceptions.NotEnoughFuel('Not Enough Fuel !!!')
+
+
+
+
+
+
+
+

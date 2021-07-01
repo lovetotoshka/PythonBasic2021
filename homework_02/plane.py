c9968eb85e3684311d82c790b6b20d9bb6c877e3
lovetotoshka <i-shkileva@mail.ru>
2021-07-01 14:52:27 +0300

    car

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -7,7 +7,8 @@ from homework_02 import base, exceptions
 class Plane(base.Vehicle):
     cargo = 0
 
-    def __init__(self, max_cargo):
+    def __init__(self, weight, fuel, fuel_consumption, max_cargo):
+        super.__init__(weight, fuel, fuel_consumption)
         self.max_cargo = max_cargo
 
     def load_cargo(self, num):

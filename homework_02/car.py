c7865111998cd44ae3d7ef848f3b0de6e0bb7ff0
lovetotoshka <i-shkileva@mail.ru>
2021-07-01 14:15:17 +0300

    car.py

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,3 +1,25 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
+from homework_02 import base
+
+
+class Car(Vehicle):
+
+    engine = 0
+
+    def __init__(self, weight, fuel, fuel_consumption):
+        super.__init__(weight, fuel, fuel_consumption)
+
+
+    def set_engine(self):
+        pass
+
+#car = Car(101,101,11)
+#print(car.weight, car.fuel, car.fuel_consumption)
+
+
+
+
+
+

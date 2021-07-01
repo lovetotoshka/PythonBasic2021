3fd426702be7897a833d97a5e0f5d10f54830bb1
lovetotoshka <i-shkileva@mail.ru>
2021-07-01 13:19:50 +0300

    base

--- homework_02/base.py
+++ homework_02/base.py
@@ -18,11 +18,13 @@ class Vehicle:
                 raise exceptions.LowFuelError('Low Fuel Error !!!')
 
     def move(self, distance):
-        if self.fuel > 0 and self.fuel_consumption and self.fuel > self.fuel_consumption:
+        if self.fuel > 0 and self.fuel_consumption > 0 and self.fuel > self.fuel_consumption:
             max_distance = self.fuel // self.fuel_consumption
             if max_distance > distance:
                 self.fuel = self.fuel - distance * self.fuel_consumption
             else:
+                raise exceptions.NotEnoughFuel('Not Enough Fuel !!!')
+        else:
                 raise exceptions.NotEnoughFuel('Not Enough Fuel !!!')
 
 

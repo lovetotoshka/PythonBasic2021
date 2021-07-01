0199bca1ab5adea550d9c5805aca95ff71ffc50e
lovetotoshka <i-shkileva@mail.ru>
2021-07-01 14:47:42 +0300

    plane

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -1,3 +1,23 @@
 """
 создайте класс `Plane`, наследник `Vehicle`
 """
+from homework_02 import base, exceptions
+
+
+class Plane(base.Vehicle):
+    cargo = 0
+
+    def __init__(self, max_cargo):
+        self.max_cargo = max_cargo
+
+    def load_cargo(self, num):
+        if self.cargo + num <= self.max_cargo:
+            self.cargo += num
+        else:
+            raise exceptions.CargoOverload('Cargo Over load !!!')
+
+    def remove_all_cargo(self):
+        yield self.cargo
+        self.cargo = 0
+
+

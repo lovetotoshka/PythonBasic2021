3658a3769e8255ddc820faf507a6c904a359c18e
lovetotoshka <i-shkileva@mail.ru>
2021-07-01 14:48:26 +0300

    car

--- homework_02/car.py
+++ homework_02/car.py
@@ -7,9 +7,5 @@ from homework_02 import base, engine
 class Car(base.Vehicle):
     engine = 0
 
-    # def __init__(self, weight, fuel, fuel_consumption):
-    #     super.__init__(weight, fuel, fuel_consumption)
-
     def set_engine(self, new_engine):
         self.engine = new_engine
-

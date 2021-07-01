db9b5e1f53e293a98966cbffe9275860255a781e
lovetotoshka <i-shkileva@mail.ru>
2021-07-01 14:36:16 +0300

    car.py and engine.py

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,25 +1,21 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
-from homework_02 import base
+from homework_02 import base, engine
 
 
-class Car(Vehicle):
-
+class Car(base.Vehicle):
     engine = 0
 
-    def __init__(self, weight, fuel, fuel_consumption):
-        super.__init__(weight, fuel, fuel_consumption)
+    # def __init__(self, weight, fuel, fuel_consumption):
+    #     super.__init__(weight, fuel, fuel_consumption)
+
+    def set_engine(self, new_engine):
+        self.engine = new_engine
 
 
-    def set_engine(self):
-        pass
-
-#car = Car(101,101,11)
-#print(car.weight, car.fuel, car.fuel_consumption)
-
-
-
-
-
-
+#car = Car(101, 101, 11)
+#print(car.weight, car.fuel, car.fuel_consumption, '!!!!!!')
+#a = engine.Engine(10, 20)
+#car.set_engine(a)
+#print (car.engine.volume, car.engine.pistons)
--- homework_02/engine.py
+++ homework_02/engine.py
@@ -6,5 +6,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Engine:
+
     volume: int
     pistons: int
+

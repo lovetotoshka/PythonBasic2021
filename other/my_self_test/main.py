7d3c0ed6bb0400df6626528a4dd985eb4bd63e0f
lovetotoshka <i-shkileva@mail.ru>
2021-07-04 16:59:13 +0300

    теперь работает с декоратором

--- other/my_self_test/main.py
+++ other/my_self_test/main.py
@@ -3,8 +3,8 @@
 import os
 from random import randint
 
-#import questions_and_answers
-#from PythonBasic2021.other.my_self_test import questions_and_answers
+# import questions_and_answers
+# from PythonBasic2021.other.my_self_test import questions_and_answers
 from other.my_self_test import questions_and_answers
 
 
@@ -21,29 +21,32 @@ def input_value_from_x_to_y(x=0, y=5):
             print(f'Вы можете ввести только числа. Ведите число от {x} до {y}')
     return score
 
+
 def select_topic_and_our_func(func):
     def inner():
         print("\nКакая тема Вас интересует?\n'1' - topic_1\n'2' - topic_2 ")
         topic = str(input_value_from_x_to_y(1, 2))
         func('topic_' + topic)
-    return inner #убрала скобки
+
+    return inner  # убрала скобки
+
 
 def all_test_on_the_right_topic(topic):
     all_score = 0
     lst = questions_and_answers.all_questions[topic]['questions']
-    for i,question in enumerate(lst):
+    for i, question in enumerate(lst):
         input(question)
         answer = questions_and_answers.all_questions[topic]['answers'][i]
         print("\nПравильный ответ: \n\t", answer, '\nОцените себя за этот вопрос по шкале от 0 до 5')
         num = input_value_from_x_to_y(0, 5)
         all_score += num
         os.system('cls')
-    print(f"Вы набрали {all_score} из {len(lst)*5}")
+    print(f"Вы набрали {all_score} из {len(lst) * 5}")
 
 
 def all_test(lst):
     all_score = 0
-    for i  in lst:
+    for i in lst:
         question, answer = i
         input(question)
         print("\nПравильный ответ: \n\t", answer, '\nОцените себя за этот вопрос по шкале от 0 до 5')
@@ -55,7 +58,6 @@ def all_test(lst):
 
 @select_topic_and_our_func
 def select_a_rand_question_in_necessary_topic(topic):
-    print ("где я ????????????????")
     questions = questions_and_answers.all_questions[topic]['questions']
     i = randint(0, len(questions) - 1)
     print(questions[i])
@@ -85,8 +87,9 @@ def add_question_and_answer_to_topic(topic):
     questions_and_answers.all_questions[topic]['questions'] += [question]
     questions_and_answers.all_questions[topic]['answers'] += [answer]
 
-def show_menu_and_select_an_item():
-        print("""
+
+def show_menu_and_select_an_action():
+    print("""
         меню:
         "1" - выбрать случайный вопрос из нужной темы
         "2" - пройти весь тест
@@ -95,26 +98,20 @@ def show_menu_and_select_an_item():
         "5" - удалить вопрос и ответ
         "6" - найти вопрос по ключевым словам
         "7" Выйти \n""")
-        num = input_value_from_x_to_y(1, 7)
-        return num
-
-
+    num = input_value_from_x_to_y(1, 7)
+    return num
 
 
 if __name__ == '__main__':
     lst = questions_and_answers.new_list()
     start = True
     while start:
-        num = show_menu_and_select_an_item()
-        print (num)
+        num = str(show_menu_and_select_an_action())
         if num == '1':
             select_a_rand_question_in_necessary_topic()
-            #print("\nКакая тема Вас интересует?\n'1' - topic_1\n'2' - topic_2 ")
-            #topic = str(input_value_from_x_to_y(1, 2))
-            #select_a_rand_question_in_necessary_topic('topic_' + topic)
-        elif num =='2':
+        elif num == '2':
             all_test(lst)
-        elif num =='3':
+        elif num == '3':
             print("\nКакая тема Вас интересует?\n'1' - topic_1\n'2' - topic_2 ")
             topic = str(input_value_from_x_to_y(1, 2))
             all_test_on_the_right_topic('topic' + topic)
@@ -124,10 +121,6 @@ if __name__ == '__main__':
             add_question_and_answer_to_topic('topic' + topic)
         elif num == '7':
             start = False
-
-
-
-
 
 '''
 1) нужно записать вопросы по правильным топикам
